66f1ec1a116fedcd726c76693b11965236d5398d
wexp <wexpmail@gmail.com>
2024-03-01 14:34:32 +0100

    Inital commit

---
+++ ExampleMod/YardAiExtended.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Railloader;
+
+namespace YardAiExtended
+{
+    public class YardAiExtended : PluginBase
+    {
+        public YardAiExtended()
+        {
+            new Harmony("wexp.YardAiExtended").PatchAll(GetType().Assembly);
+        }
+    }
+}
---
+++ ExampleMod/YardExtender.cs
@@ -0,0 +1,137 @@
+﻿using Game.Messages;
+using Game.State;
+using HarmonyLib;
+using Model;
+using Model.AI;
+using System;
+using System.Reflection;
+using UI.Builder;
+using UI.CarInspector;
+using UnityEngine;
+
+
+namespace YardAiExtended
+{
+    [HarmonyPatch(typeof(CarInspector), "PopulateAIPanel")]
+    public static class AIPanelExtender
+    {
+        static void Postfix(UIPanelBuilder builder, CarInspector __instance, Car ____car)
+        {
+            builder.FieldLabelWidth = 100f;
+            builder.Spacing = 8f;
+            AutoEngineerPersistence persistence = new AutoEngineerPersistence(____car.KeyValueObject);
+
+            {
+                var aimode = Mode();
+                int num = MaxSpeedMphForMode(aimode);
+
+                RectTransform control2 = builder.ButtonStrip(delegate (UIPanelBuilder builder1)
+                {
+                    builder1.AddButton("30", delegate
+                    {
+                        float? distance25 = 366f;
+                        SetOrdersValue(null, null, null, distance25);
+                    });
+                    builder1.AddButton("40", delegate
+                    {
+                        float? distance25 = 488f;
+                        SetOrdersValue(null, null, null, distance25);
+                    });
+                    builder1.AddButton("50", delegate
+                    {
+                        float? distance25 = 610f;
+                        SetOrdersValue(null, null, null, distance25);
+                    });
+                    builder1.AddButton("70", delegate
+                    {
+                        float? distance25 = 854f;
+                        SetOrdersValue(null, null, null, distance25);
+                    });
+                    builder1.AddButton("100", delegate
+                    {
+                        float? distance25 = 1220f;
+                        SetOrdersValue(null, null, null, distance25);
+                    });
+                    builder1.AddButton("200", delegate
+                    {
+                        float? distance25 = 2440f;
+                        SetOrdersValue(null, null, null, distance25);
+                    });
+                });
+
+                builder.AddField("YAE", control2);
+
+
+                /*     RectTransform control3 = builder.AddSlider(() => persistence.Orders.MaxSpeedMph, delegate
+                     {
+                         int maxSpeedMph25 = persistence.Orders.MaxSpeedMph;
+                         return maxSpeedMph25.ToString(); 
+                     }, delegate (float value)
+                     {
+                         int? maxSpeedMph25 = (int)(value);
+                         SetOrdersValue(null, null, maxSpeedMph25, null);
+                     }, 0f, num, wholeNumbers: true);
+                     builder.AddField("Max Yard Speed", control3);
+                 */
+            }
+
+            void SendAutoEngineerCommand(AutoEngineerMode mode, bool forward, int maxSpeedMph, float? distance)
+            {
+                StateManager.ApplyLocal(new AutoEngineerCommand(____car.id, mode, forward, maxSpeedMph, distance));
+            }
+
+            AutoEngineerMode Mode()
+            {
+                Orders orders2 = persistence.Orders;
+                if (!orders2.Enabled)
+                {
+                    return AutoEngineerMode.Off;
+                }
+                if (!orders2.Yard)
+                {
+                    return AutoEngineerMode.Road;
+                }
+                return AutoEngineerMode.Yard;
+            }
+
+            int MaxSpeedMphForMode(AutoEngineerMode mode)
+            {
+                switch (mode)
+                {
+                    case AutoEngineerMode.Road:
+                        return 45;
+                    case AutoEngineerMode.Yard:
+                        return 25;
+                    case AutoEngineerMode.Off:
+                        return 0;
+                    default:
+                        throw new ArgumentOutOfRangeException("mode", mode, null);
+                }
+            }
+
+
+            void SetOrdersValue(AutoEngineerMode? mode, bool? forward, int? maxSpeedMph, float? distance)
+            {
+                Orders orders = persistence.Orders;
+                if (!orders.Enabled && mode.HasValue && mode.Value != 0 && !maxSpeedMph.HasValue)
+                {
+                    float num2 = ____car.velocity * 2.23694f;
+                    float num3 = Mathf.Abs(num2);
+                    maxSpeedMph = ((num2 > 0.1f) ? (Mathf.CeilToInt(num3 / 5f) * 5) : 0);
+                    forward = num2 >= -0.1f;
+                }
+                if (mode == AutoEngineerMode.Yard)
+                {
+                    maxSpeedMph = MaxSpeedMphForMode(AutoEngineerMode.Yard);
+                }
+                AutoEngineerMode mode3 = mode ?? 0;
+                int maxSpeedMph2 = Mathf.Min(maxSpeedMph ?? orders.MaxSpeedMph, MaxSpeedMphForMode(mode3));
+                SendAutoEngineerCommand(mode3, forward ?? orders.Forward, maxSpeedMph2, distance);
+            }
+        }
+
+
+    }
+
+
+}
